95bece38a86f03def6bfca9171398fea4bb3545f
jugalkpatel <ptjimmy99@gmail.com>
2020-12-23 15:02:16 +0530

    Mini Project 2 Complete

---
+++ 190423116007_MiniProject2/scripts/index.js
@@ -0,0 +1,24 @@
+let output = document.querySelector('.output');
+const plus = document.querySelector('.plus');
+const minus = document.querySelector('.minus');
+
+let counter = Number(output.innerText);
+
+plus.addEventListener('click', function() {
+    counter += 1;
+    output.innerText = counter;
+    checkCount(counter);
+})
+minus.addEventListener('click', function(){
+    counter -= 1;
+    output.innerText = counter;
+    checkCount(counter);
+})
+
+function checkCount(digit) {
+    if (digit > 0) {
+        output.style.color = "green";
+        return;
+    }
+    output.style.color = "red";
+}
