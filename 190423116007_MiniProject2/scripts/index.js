09b3531814dbc325c76ff3d455ee4a68632f7002
jugalkpatel <ptjimmy99@gmail.com>
2020-12-28 11:09:02 +0530

    refactor: organize files and folders

--- 190423116007_MiniProject2/scripts/index.js
+++ 190423116007_MiniProject2/scripts/index.js
@@ -1,7 +1,7 @@
 let output = document.querySelector('.output');
 const plus = document.querySelector('.plus');
 const minus = document.querySelector('.minus');
-
+const reset = document.querySelector('.btn-reset');
 let counter = Number(output.innerText);
 
 plus.addEventListener('click', function(e) {
@@ -13,6 +13,10 @@ minus.addEventListener('click', function(e){
     counter -= 1;
     output.innerText = counter;
     checkCount(counter);
+})
+
+reset.addEventListener('click', function(e){
+    output.innerText = "0";
 })
 
 function checkCount(digit) {
