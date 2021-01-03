98ba34c33aa968ebd2b7e052695ee90df84aaa34
jugalkpatel <ptjimmy99@gmail.com>
2021-01-03 20:41:30 +0530

    Completed Mini Project 3

--- MiniProjects/190423116007_MiniProject3/index.js
+++ MiniProjects/190423116007_MiniProject3/index.js
@@ -1,8 +1,43 @@
-function displayTime() {
-  let date = new Date();
-  let time = date.toLocaleTimeString();
-  document.querySelector(".clock").textContent = time;
+let secondCount = 0;
+
+let stopWatch;
+
+const displayPara = document.querySelector(".clock");
+
+function displayCount() {
+  let hours = Math.floor(secondCount / 3600);
+  let minutes = Math.floor((secondCount % 3600) / 60);
+  let seconds = Math.floor(secondCount % 60);
+
+  let displayHours = hours < 10 ? "0" + hours : hours;
+  let displayMinutes = minutes < 10 ? "0" + minutes : minutes;
+  let displaySeconds = seconds < 10 ? "0" + seconds : seconds;
+
+  displayPara.textContent =
+    displayHours + ":" + displayMinutes + ":" + displaySeconds;
+
+  secondCount++;
 }
 
-displayTime();
-const createClock = setInterval(displayTime, 1000);
+const startBtn = document.querySelector(".start-btn");
+const stopBtn = document.querySelector(".stop-btn");
+const resetBtn = document.querySelector(".reset");
+
+startBtn.addEventListener("click", () => {
+  stopWatch = setInterval(displayCount, 1000);
+  startBtn.disabled = true;
+});
+
+stopBtn.addEventListener("click", () => {
+  clearInterval(stopWatch);
+  startBtn.disabled = false;
+});
+
+resetBtn.addEventListener("click", () => {
+  clearInterval(stopWatch);
+  startBtn.disabled = false;
+  secondCount = 0;
+  displayCount();
+});
+
+displayCount();
